88a4bc42b5ad92f246e93083b2fe5700a51bfaa0
Irina Saf <irina.safronova0801@gmail.com>
2025-11-05 11:25:25 -0500

    Initial Commit

---
+++ MediaPlannerApp/MediaPlannerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaPlannerAppApp.swift
+//  MediaPlannerApp
+//
+//  Created by Irina Saf on 2025-11-05.
+//
+
+import SwiftUI
+
+@main
+struct MediaPlannerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
